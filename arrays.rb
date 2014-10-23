d34f3e4f5956a429808d1c622634b90a498bebfd
Marek Grabowski <grabowskimark@outlook.com>
2014-10-23 12:04:38 +0200

    Tutorial

---
+++ arrays.rb
@@ -0,0 +1,39 @@
+# Tablice w Ruby 
+
+# To jest tablica. Tak samo jak szkolna - przechowuje dane.
+array = [1, 2, 3, 4, 5] #=> [1, 2, 3, 4, 5]
+
+# Tablice zawieraja rozne typy danych. Na przykład dwa lisy. Takie prawdziwe.
+[2, 'lis', true] #=> [2, "lis", true] (int, string, boolean)
+
+# Incepcja...
+tab =[ [1,2], [3,4], [5,6] ]
+
+# Tablice maja indeksy. (takie studenckie)
+# Numerujemy je od początku (zera)...
+array[0] #=> 1
+array[10] #=> nil
+
+# i od konca.
+array[-1] #=> 5
+
+# Mozna tez podac indeks poczatkowy i dlugosc...
+array[3, 2] #=> [4, 5]
+
+# lub zasieg.
+array[1..4] #=> [2, 3, 4 5]
+
+# Chcesz cos dodac? Zaden problem!
+array << 6 #=> [1, 2, 3, 4, 5, 6]
+
+# Tablica tak jak wszystko w Ruby jest obiektem.
+# Wiec czemu by nie pobawic sie metodami...
+array.[] 1 #=> 2
+array.[] 100 #=> nil
+# i wyswietlic element tablicy w nieco bardziej obiektowy sposob?
+
+
+
+
+
+
